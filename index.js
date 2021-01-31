f6db0c6abd993ff26f31b06237e32f9062d6ec70
Arvin Fernandez <arvinf07@gmail.com>
2021-01-30 20:55:06 -0500

    commit tree 554697fc320d873650e53d3a71be23b84684710b

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
 // Your code goes here
+let paragraph = document.querySelector('p#text')
+
+document.addEventListener("DOMContentLoaded", function() {
+  paragraph.innerHTML = "This is really cool!";
+});
+
